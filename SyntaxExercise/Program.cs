65006d65d8fab40627f594f0cc0465b14b8f87e1
Stephen Madalo <stephenmadalo89@gmail.com>
2022-04-30 11:37:06 -0400

    Initial commit

---
+++ SyntaxExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxExercise 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 9;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"; 
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
